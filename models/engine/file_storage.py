64e706e54e1527510d268278a7273efe9565005c
charles ezebuike <ezebuike.charles0@gmail.com>
2023-03-22 20:08:45 +0000

    swap

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,63 +1,86 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
+"""This is the file storage class for AirBnB"""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
-    __file_path = 'file.json'
+    """This class serializes instances to a JSON file and
+    deserializes JSON file to instances
+    Attributes:
+        __file_path: path to the JSON file
+        __objects: objects will be stored
+    """
+    all_classes = {"BaseModel", "User", "State", "City",
+                   "Amenity", "Place", "Review"}
+    __file_path = "file.json"
     __objects = {}
 
     def all(self, cls=None):
-        """Returns the list of objects of one type of class"""
-        dic = {}
-        if cls:
-            for key, value in self.__objects.items():
-                if cls.__name__ in key:
-                    dic[key] = value
-                    return dic
-        return self.__objects
+        """returns a dictionary
+        Return:
+            returns a dictionary of __object"""
+        if cls is None:
+            return self.__objects
+        if type(cls) is str:
+            cls = eval(cls)
+
+        filtered_dict = {}
+        cls_name = cls.__name__
+        if cls_name in self.all_classes:
+            for k in self.__objects.keys():
+                sp = k.split(".")
+                if sp[0] == cls_name:
+                    filtered_dict[k] = self.__objects[k]
+        return filtered_dict
+
+    def delete(self, obj=None):
+        """delete obj
+        Return:
+            nothing
+        """
+        if obj is None:
+            return
+        else:
+            obj_key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            if obj_key in self.__objects.keys():
+                del self.__objects[obj_key]
 
     def new(self, obj):
-        """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        """sets __object to given obj
+        Args:
+            obj: given object
+        """
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            self.__objects[key] = obj
 
     def save(self):
-        """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
-            json.dump(temp, f)
+        """serialize the file path to JSON file path
+        """
+        my_dict = {}
+        for key, value in self.__objects.items():
+            my_dict[key] = value.to_dict()
+        with open(self.__file_path, 'w', encoding="UTF-8") as f:
+            json.dump(my_dict, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+        """serialize the file path to JSON file path
+        """
         try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
+            with open(self.__file_path, 'r', encoding="UTF-8") as f:
+                for key, value in (json.load(f)).items():
+                    value = eval(value["__class__"])(**value)
+                    self.__objects[key] = value
         except FileNotFoundError:
             pass
 
-    def delete(self, obj=None):
-        """Delete a given object from __objects, if it exists."""
-        try:
-            del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
-        except (AttributeError, KeyError):
-            pass
+    def close(self):
+        """calls reload() deserializes objects"""
+        self.reload()
--- models/place.py
+++ models/place.py
@@ -1,67 +1,79 @@
 #!/usr/bin/python3
-"""Defines the Place class."""
+"""This is the place class"""
+from models.base_model import Base, BaseModel
 import models
-from os import getenv
-from models.base_model import Base
-from models.base_model import BaseModel
-from models.amenity import Amenity
-from models.review import Review
-from sqlalchemy import Column
-from sqlalchemy import Float
-from sqlalchemy import ForeignKey
-from sqlalchemy import Integer
-from sqlalchemy import String
-from sqlalchemy import Table
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, Table
 from sqlalchemy.orm import relationship
-
-
-association_table = Table("place_amenity", Base.metadata,
-                          Column("place_id", String(60),
-                                 ForeignKey("places.id"),
-                                 primary_key=True, nullable=False),
-                          Column("amenity_id", String(60),
-                                 ForeignKey("amenities.id"),
-                                 primary_key=True, nullable=False))
+from os import getenv
 
 
 class Place(BaseModel, Base):
-    """Represents a Place for a MySQL database"""
-    __tablename__ = "places"
-    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    """This is the class for Place
+    Attributes:
+        city_id: city id
+        user_id: user id
+        name: name input
+        description: string of description
+        number_rooms: number of room in int
+        number_bathrooms: number of bathrooms in int
+        max_guest: maximum guest in int
+        price_by_night:: pice for a staying in int
+        latitude: latitude in flaot
+        longitude: longitude in float
+        amenity_ids: list of Amenity ids
+    """
+    __tablename__ = 'places'
+
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024))
-    number_rooms = Column(Integer, default=0)
-    number_bathrooms = Column(Integer, default=0)
-    max_guest = Column(Integer, default=0)
-    price_by_night = Column(Integer, default=0)
-    latitude = Column(Float)
-    longitude = Column(Float)
-    reviews = relationship("Review", backref="place", cascade="delete")
-    amenities = relationship("Amenity", secondary="place_amenity",
-                             viewonly=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     amenity_ids = []
 
-    if getenv("HBNB_TYPE_STORAGE", None) != "db":
-        @property
-        def reviews(self):
-            """Get a list of all linked Reviews."""
-            review_list = []
-            for review in list(models.storage.all(Review).values()):
-                if review.place_id == self.id:
-                    review_list.append(review)
-            return review_list
+    place_amenity = Table('place_amenity', Base.metadata, Column(
+                'place_id', String(60), ForeignKey(
+                    'places.id'), primary_key=True, nullable=False), Column(
+                    'amenity_id', String(60), ForeignKey(
+                        'amenities.id'), primary_key=True, nullable=False))
 
+    reviews = relationship(
+            "Review", backref='place', cascade="all, delete, delete-orphan")
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        amenities = relationship(
+                "Amenity", secondary='place_amenity', viewonly=False)
+
+    else:
         @property
         def amenities(self):
-            """Get/set linked Amenities."""
-            amenity_list = []
-            for amenity in list(models.storage.all(Amenity).values()):
-                if amenity.id in self.amenity_ids:
-                    amenity_list.append(amenity)
-            return amenity_list
+            """ return list of amenities that certain place has"""
+            all_objs = models.storage.all()
+            amenities_list = []
+            for k, v in all_objs.items():
+                if v.__class__.__name__ == 'Amenity' and \
+                                         v.id in self.amenity_ids:
+                    amenities_list.append(v)
+            return amenities_list
 
         @amenities.setter
-        def amenities(self, value):
-            if type(value) == Amenity:
-                self.amenity_ids.append(value.id)
+        def amenties(self):
+            """ add Amenity.id to amenity_ids list"""
+            if self.__class__.__name__ is not 'Amenity':
+                return
+            amenity_ids.append(self.id)
+
+        @property
+        def reviews(self):
+            """ gets a list of review objects from that place """
+            all_obj = models.storage.all()
+            reviews_list = []
+            for k, v in all_obj.items():
+                if v.__class__.__name__ == 'Review' and v.place_id == self.id:
+                    reviews_list.append(v)
+            return reviews_list
