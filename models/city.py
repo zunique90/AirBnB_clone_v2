d00e4ae99ed59e4a77f7d2c91b1edf3d8cccf439
charles ezebuike <ezebuike.charles0@gmail.com>
2023-03-22 19:39:37 +0000

    corrections

--- models/city.py
+++ models/city.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
+"""Defines the City class."""
 from models.base_model import Base
 from models.base_model import BaseModel
 from sqlalchemy import Column
@@ -9,7 +9,7 @@ from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """Represents a City"""
+    """Represents a city for a MySQL database"""
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
--- models/user.py
+++ models/user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+"""Defines the User class."""
 from models.base_model import Base
 from models.base_model import BaseModel
 from sqlalchemy import Column
@@ -8,7 +8,7 @@ from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
+    """Represents a user for a MySQL database"""
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
