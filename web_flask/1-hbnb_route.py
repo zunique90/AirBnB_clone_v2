a820d49abcb3dcee1270e1f7e45502a20c33c0df
charles ezebuike <ezebuike.charles0@gmail.com>
2023-04-23 13:36:31 +0000

    satisfied pycodestyle

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -13,10 +13,12 @@ def hello():
     """displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """displays 'HBNB'"""
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
